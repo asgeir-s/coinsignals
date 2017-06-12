2783475d757429a49d205679dabff61146df2011
Asgeir Sognefest <asgeir.sognefest@acando.no>
2017-06-12 22:32:11 +0200

    Add components and dummy data to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Position } from "./types/Position.d";
 
 @Component({
-  selector: 'app-root',
+  selector: 'cs-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works!'; 
+
+  dummyPositions: Position[] = [
+    {
+      coin: "bitcoin",
+      amount: 4,
+      timestamp: 143323232232,
+      closed: false
+    },
+    {
+      coin: "ether",
+      amount: 40,
+      timestamp: 143323232333,
+      closed: false
+    }
+  ]
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
 
 import { AppComponent } from './app.component';
+import { PositionListComponent } from './position-list/position-list.component';
+import { MaterialModule } from "./material/material.module";
+import { StreamListComponent } from './stream-list/stream-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PositionListComponent,
+    StreamListComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    NgxDatatableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
